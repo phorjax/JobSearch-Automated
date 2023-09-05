a03630072f399c89e50c051b86d002dedf3d90f5
anthony gomez <joseph.12082@gmail.com>
2023-09-05 17:14:08 -0400

    bot functional, applying and paginating. might need some debugging features and enhancements

--- main.py
+++ main.py
@@ -39,60 +39,91 @@ def scrolling():
     driver.execute_script(scroll_up, jobs_listing_body)
 
 # def pagination():
-    
+#     # find_pages = driver.find_element(By.CLASS_NAME, 'artdeco-pagination__pages')
 
+#     # page_buttons = find_pages.find_element(By.TAG_NAME, 'button')
+#     # print(len(page_buttons))
+#     for i in range(1, 40):
+#         scrolling()
+#         next_button = driver.find_element(By.XPATH, "//button[span[text()='" + str(i + 1) + "']]")        
+#         next_button.click()
+#         time.sleep(1)
+
+        
+#         continue
+        
+        
 def get_and_apply_to_listings():
-    scrolling()
-    all_listings = driver.find_elements(By.CLASS_NAME, 'job-card-container--clickable')
-    for listing in all_listings:
-        listing.click()
-        time.sleep(2)
-        try:
-            apply_button = driver.find_element(By.CLASS_NAME, 'jobs-apply-button')
-            if apply_button.get_attribute('role') == 'link':
-                continue
-            apply_button.click()
-            time.sleep(1)
-            footer_button = driver.find_element(By.CSS_SELECTOR, 'footer button')
-            footer_button.click()
-            time.sleep(1)
-            spans = driver.find_elements(By.TAG_NAME, 'span')
-            next_exists = False
-            post_apply_button_exists = False
-            for span in spans:
-                if span.text == 'Next':
-                    next_exists = True
-                    break
-                if span.text == 'Add Skill' or span.text == 'Get started' or span.text == 'Done' or span.text == 'Answer questions':
-                    post_apply_button_exists = True
-            if post_apply_button_exists == True:
-                x_button = driver.find_element(By.CLASS_NAME, 'artdeco-modal__dismiss')
-                x_button.click()
+        for i in range(1, 40):
+            scrolling()
+            all_listings = driver.find_elements(By.CLASS_NAME, 'job-card-container--clickable')
+            for listing in all_listings:
+                listing.click()
+                time.sleep(2)
+                try:
+                    apply_button = driver.find_element(By.CLASS_NAME, 'jobs-apply-button')
+                    if apply_button.get_attribute('role') == 'link':
+                        continue
+                    apply_button.click()
+                    time.sleep(1)
+                    footer_button = driver.find_element(By.CSS_SELECTOR, 'footer button')
+                    footer_button.click()
+                    time.sleep(1)
+                    spans = driver.find_elements(By.TAG_NAME, 'span')
+                    next_exists = False
+                    post_apply_button_exists = False
+                    for span in spans:
+                        if span.text == 'Next':
+                            next_exists = True
+                            break
+                        if span.text == 'Add Skill' or span.text == 'Get started' or span.text == 'Done' or span.text == 'Answer questions':
+                            post_apply_button_exists = True
+                            print('job applied successfully')
+                            break
+                    if post_apply_button_exists == True:
+                        x_button = driver.find_element(By.CLASS_NAME, 'artdeco-modal__dismiss')
+                        x_button.click()
+                        time.sleep(1)
+                        continue
+                    if next_exists == True:
+                        x_button = driver.find_element(By.CLASS_NAME, 'artdeco-modal__dismiss')
+                        x_button.click()
+                        time.sleep(1)
+                        discard_button = driver.find_element(By.XPATH, "//button[span[text()='Discard']]")
+                        discard_button.click()
+                        time.sleep(1)
+                    try_premium_button_to_check = driver.find_element(By.TAG_NAME, 'a').text
+                    if try_premium_button_to_check == 'Try Premium for $0':
+                        print('job applied successfully')
+                        x_button = driver.find_element(By.CLASS_NAME, 'artdeco-modal__dismiss')
+                        x_button.click()
+                        time.sleep(1)
+                        continue
+                    footer_button = driver.find_element(By.XPATH, "//button[span[text()='Review']]")
+                    footer_button.click()
+                    time.sleep(1)
+                    submit_button = driver.find_element(By.XPATH, "//button[span[text()='Submit application']]")
+                    submit_button.click()
+                    print('job applied successfully')
+
+                    time.sleep(1)
+                    x_button = driver.find_element(By.CLASS_NAME, 'artdeco-modal__dismiss')
+                    x_button.click()
+                    time.sleep(1)
+                    continue
+                except NoSuchElementException:
+                    continue
+            try:
+                next_button = driver.find_element(By.XPATH, "//button[span[text()='" + str(i + 1) + "']]")        
+                next_button.click()
                 time.sleep(1)
                 continue
-            if next_exists == True:
-                x_button = driver.find_element(By.CLASS_NAME, 'artdeco-modal__dismiss')
-                x_button.click()
+            except NoSuchElementException:
+                next_button = driver.find_element(By.XPATH, "//button[span[text()='…']]")
+                next_button.click()
                 time.sleep(1)
-                discard_button = driver.find_element(By.XPATH, "//button[span[text()='Discard']]")
-                discard_button.click()
-                time.sleep(1)
-            footer_button = driver.find_element(By.XPATH, "//button[span[text()='Review']]")
-            footer_button.click()
-            time.sleep(1)
-            submit_button = driver.find_element(By.XPATH, "//button[span[text()='Submit application']]")
-            submit_button.click()
-            time.sleep(1)
-            x_button = driver.find_element(By.CLASS_NAME, 'artdeco-modal__dismiss')
-            x_button.click()
-            time.sleep(1)
-            continue
-        except NoSuchElementException:
-            continue
-    
+                continue         
 get_and_apply_to_listings()    
-    
-    
     
     
     
